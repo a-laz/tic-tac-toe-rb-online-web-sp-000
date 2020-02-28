f1bf2a49d8c43aa94e0a5d4c3afbe41a1d0453bf
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-02-28 15:19:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -119,18 +119,18 @@ def draw?(board)
 end
 
 def over?(board)
-  #draw?(board) || won?(board)
+  draw?(board) || won?(board)
   #binding.pry
-  if draw?(board) == true
-    return true
-  elsif full?(board) == true && won?(board).class == Array
-    return true
-  elsif won?(board).class == Array && full?(board) == false
-    return true
-  else
-    return false
-  end
-end
+#  if draw?(board) == true
+#    return true
+#  elsif full?(board) == true && won?(board).class == Array
+#    return true
+#  elsif won?(board).class == Array && full?(board) == false
+#    return true
+#  else
+#    return false
+#  end
+#end
 
 def winner(board)
   if won?(board) == nil
